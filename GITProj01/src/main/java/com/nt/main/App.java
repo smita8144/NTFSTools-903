ac74fd26bb9405337ab695a95b324a1020f719f2
DELL <DELL@DESKTOP-C45DOSR>
2024-11-09 13:55:46 +0530

    TL change 44

--- GITProj01/src/main/java/com/nt/main/App.java
+++ GITProj01/src/main/java/com/nt/main/App.java
@@ -10,6 +10,7 @@ public class App {
     public static void main(String[] args) {
         App app=new App();
         System.out.println("sum result22 is::"+app.sum(10, 20));
-        System.out.println("sum amount33 is::"+app.sum(10, 20));
+        System.out.println("sum amount44 is::"+app.sum(10, 20));
+        //hello
     }
 }
