bc659fde409ae8c0fd0403b97641c264a25238b2
DELL <DELL@DESKTOP-C45DOSR>
2024-11-11 16:54:32 +0530

    Dev1--UPIPayment devlopment

---
+++ GITProj01/src/main/java/com/nt/upi/UPIPayment.java
@@ -0,0 +1,11 @@
+package com.nt.upi;
+
+public class UPIPayment {
+	
+	public String doUPIPayment(String upiId,float amount) {
+		return "UPI Payment for the amount::"+amount+"is done";
+		
+	}
+	
+
+}
