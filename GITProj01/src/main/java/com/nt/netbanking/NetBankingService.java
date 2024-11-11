05351637a75bdaa92ff125c51ff210d03eda2705
DELL <DELL@DESKTOP-C45DOSR>
2024-11-11 18:14:34 +0530

    Dev1--close account

--- GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
@@ -7,4 +7,7 @@ public class NetBankingService {
 	public String openAccount(String addhar,double initialamount,String type) {
 		return "account is opened with acc number::"+new Random().nextLong(50000000000L);
 	}
+	public String closeAccount() {
+		return"Account is closed";
+	}
 }
