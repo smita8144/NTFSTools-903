21162369b9533c73df7d179e46c26dbc4ad7ecf5
DELL <DELL@DESKTOP-C45DOSR>
2024-11-11 17:43:55 +0530

    Dev1--- account opening logic

--- GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
@@ -1,5 +1,10 @@
 package com.nt.netbanking;
 
+import java.util.Random;
+
 public class NetBankingService {
 
+	public String openAccount(String addhar,double initialamount,String type) {
+		return "account is opened with acc number::"+new Random().nextLong(50000000000L);
+	}
 }
